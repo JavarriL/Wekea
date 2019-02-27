45c6029b797b925a261f84b5e9cf4d3ed3a450fc
Cappaert <cappa1pt@cmich.edu>
2019-02-27 11:33:56 -0500

    added basic models for product searching

---
+++ Wekea/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wekea.Model
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public string description { get; set; }
+        public string imageURL { get; set; }
+        public List<Review> reviews { get; set; }
+    }
+}
---
+++ Wekea/Model/ProductSearchInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wekea.Model
+{
+    interface ProductSearchInterface
+    {
+        List<Product> Search(SearchInfo si);
+        Product getProduct(int id);
+    }
+}
